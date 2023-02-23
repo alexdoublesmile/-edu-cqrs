15d84320ba573fe69c27930974f01930377a2625
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 18:18:13 +0300

    add create event handler to save

---
+++ src/main/java/edu/joyful/cqrs/command/api/event/ProductEventsHandler.java
@@ -0,0 +1,22 @@
+package edu.joyful.cqrs.command.api.event;
+
+import edu.joyful.cqrs.command.api.model.Product;
+import edu.joyful.cqrs.command.api.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductEventsHandler {
+    private final ProductRepository productRepository;
+
+    @EventHandler
+    public void on(ProductCreateEvent event) {
+        final Product product = new Product();
+        BeanUtils.copyProperties(event, product);
+
+        productRepository.save(product);
+    }
+}
