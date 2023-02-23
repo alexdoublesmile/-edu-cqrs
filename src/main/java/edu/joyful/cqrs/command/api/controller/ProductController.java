9cb46285586e36cd9a887c33ecfd55379d49d389
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 17:17:35 +0300

    add controller

---
+++ src/main/java/edu/joyful/cqrs/command/api/controller/ProductController.java
@@ -0,0 +1,17 @@
+package edu.joyful.cqrs.command.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    @PostMapping
+    public String addProduct() {
+        return "Product added";
+    }
+}
