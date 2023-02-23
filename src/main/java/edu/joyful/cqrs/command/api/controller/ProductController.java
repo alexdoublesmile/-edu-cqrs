b3adda73103cd8cc3346301e9d9ea650a779b85b
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 17:26:25 +0300

    add controller mapping model to command and send

--- src/main/java/edu/joyful/cqrs/command/api/controller/ProductController.java
+++ src/main/java/edu/joyful/cqrs/command/api/controller/ProductController.java
@@ -1,5 +1,6 @@
 package edu.joyful.cqrs.command.api.controller;
 
+import edu.joyful.cqrs.command.api.command.CreateProductCommand;
 import edu.joyful.cqrs.command.api.model.ProductRestModel;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -17,6 +20,15 @@ public class ProductController {
 
     @PostMapping
     public String addProduct(@RequestBody ProductRestModel productRestModel) {
-        return "Product added";
+        final CreateProductCommand command = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .name(productRestModel.getName())
+                .price(productRestModel.getPrice())
+                .quantity(productRestModel.getQuantity())
+                .build();
+
+        final String result = commandGateway.sendAndWait(command);
+        
+        return result;
     }
 }
