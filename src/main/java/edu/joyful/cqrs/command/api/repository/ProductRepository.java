b865666322a6c41d168fa145c8578554d6c3c4c3
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 18:12:10 +0300

    change product repo id to String

--- src/main/java/edu/joyful/cqrs/command/api/repository/ProductRepository.java
+++ src/main/java/edu/joyful/cqrs/command/api/repository/ProductRepository.java
@@ -3,7 +3,5 @@ package edu.joyful.cqrs.command.api.repository;
 import edu.joyful.cqrs.command.api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
