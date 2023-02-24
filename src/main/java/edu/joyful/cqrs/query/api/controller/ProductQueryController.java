81ba83decf80b0387d8dd9d080a11e0e83bad802
Александр Плохой <alexplohoy@mail.ru>
2023-02-24 18:08:53 +0300

    add query controller

---
+++ src/main/java/edu/joyful/cqrs/query/api/controller/ProductQueryController.java
@@ -0,0 +1,24 @@
+package edu.joyful.cqrs.query.api.controller;
+
+import edu.joyful.cqrs.command.api.model.dto.ProductRestModel;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductQueryController {
+
+    private final QueryGateway queryGateway;
+
+    @GetMapping
+    public List<ProductRestModel> getAllProducts() {
+        
+        return null;
+    }
+}
