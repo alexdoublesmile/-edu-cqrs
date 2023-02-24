e347b6f7b90e638c9f55b4ba9fd36f197e181cc9
Александр Плохой <alexplohoy@mail.ru>
2023-02-24 20:02:51 +0300

    add error handler processing group to event handler

--- src/main/java/edu/joyful/cqrs/command/api/model/event/ProductEventsHandler.java
+++ src/main/java/edu/joyful/cqrs/command/api/model/event/ProductEventsHandler.java
@@ -3,12 +3,14 @@ package edu.joyful.cqrs.command.api.model.event;
 import edu.joyful.cqrs.command.api.model.entity.Product;
 import edu.joyful.cqrs.command.api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@ProcessingGroup("product")
 public class ProductEventsHandler {
     private final ProductRepository productRepository;
 
