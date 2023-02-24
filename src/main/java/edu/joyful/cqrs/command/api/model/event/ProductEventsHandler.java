fb23c865b53f4dd5ba7ab47266493da152e41a3c
Александр Плохой <alexplohoy@mail.ru>
2023-02-24 20:04:24 +0300

    add exception handler to event handler

--- src/main/java/edu/joyful/cqrs/command/api/model/event/ProductEventsHandler.java
+++ src/main/java/edu/joyful/cqrs/command/api/model/event/ProductEventsHandler.java
@@ -5,6 +5,7 @@ import edu.joyful.cqrs.command.api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +21,10 @@ public class ProductEventsHandler {
         BeanUtils.copyProperties(event, product);
 
         productRepository.save(product);
+    }
+
+    @ExceptionHandler
+    public void handle(Exception exception) throws Exception {
+        throw exception;
     }
 }
