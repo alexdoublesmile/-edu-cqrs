1b5855b255a9af98e811437ade5db6bccd6701c3
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:59:58 +0300

    add order-service order saving in handler

--- order-service/src/main/java/edu/joyful/orderservice/command/api/model/event/OrderEventsHandler.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/event/OrderEventsHandler.java
@@ -1,13 +1,20 @@
 package edu.joyful.orderservice.command.api.model.event;
 
+import edu.joyful.orderservice.command.api.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderEventsHandler {
+
+    private final OrderRepository orderRepository;
 
     @EventHandler
     public void on(OrderCreatedEvent event) {
 
+
+        orderRepository.save(order);
     }
 }
