c87e404e4b34d4906dc4496df7e3cd736260b2fe
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:36:11 +0300

    add order-service controller

--- order-service/src/main/java/edu/joyful/orderservice/command/api/controller/OrderCommandController.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/controller/OrderCommandController.java
@@ -1,4 +1,19 @@
 package edu.joyful.orderservice.command.api.controller;
 
+import edu.joyful.orderservice.command.api.model.OrderRestModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderCommandController {
+
+    @PostMapping
+    public String createOrder(@RequestBody OrderRestModel order) {
+        return "Order created";
+    }
 }
