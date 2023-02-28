9b7d4faf50656e14c740cd0fe50578fe09c362d7
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:46:26 +0300

    add order-service event apply

--- order-service/src/main/java/edu/joyful/orderservice/command/api/model/command/OrderAggregate.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/command/OrderAggregate.java
@@ -2,6 +2,7 @@ package edu.joyful.orderservice.command.api.model.command;
 
 import edu.joyful.orderservice.command.api.model.event.OrderCreatedEvent;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.springframework.beans.BeanUtils;
 
@@ -22,13 +23,8 @@ public class OrderAggregate {
     public OrderAggregate(CreateOrderCommand command) {
         final OrderCreatedEvent event = new OrderCreatedEvent();
 
-        BeanUtils.copyProperties();
+        BeanUtils.copyProperties(command, event);
 
-        this.orderId = command.getOrderId();
-        this.productId = command.getProductId();
-        this.userId = command.getUserId();
-        this.addressId = command.getAddressId();
-        this.quantity = command.getQuantity();
-        this.orderStatus = command.getOrderStatus();
+        AggregateLifecycle.apply(event);
     }
 }
