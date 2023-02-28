bf1f7feeb301bca0d018bc8294c801eda00c4e05
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:41:12 +0300

    add order-service command

--- order-service/src/main/java/edu/joyful/orderservice/command/api/controller/OrderCommandController.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/controller/OrderCommandController.java
@@ -1,12 +1,15 @@
 package edu.joyful.orderservice.command.api.controller;
 
 import edu.joyful.orderservice.command.api.model.OrderRestModel;
+import edu.joyful.orderservice.command.api.model.command.CreateOrderCommand;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.UUID.randomUUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -16,7 +19,14 @@ public class OrderCommandController {
 
     @PostMapping
     public String createOrder(@RequestBody OrderRestModel order) {
-        CreateOrderCommand command;
+        CreateOrderCommand command = CreateOrderCommand.builder()
+                .orderId(randomUUID().toString())
+                .addressId(order.getAddressId())
+                .productId(order.getProductId())
+                .quantity(order.getQuantity())
+                .userId(order.getUserId())
+                .orderStatus()
+                .build();
 
         gateway.sendAndWait(command);
 
