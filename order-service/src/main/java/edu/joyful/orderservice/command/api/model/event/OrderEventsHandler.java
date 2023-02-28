9665821eb1b4efe663d255d234efd2d24cbf63f8
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:56:14 +0300

    add order-service create event handler

---
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/event/OrderEventsHandler.java
@@ -0,0 +1,13 @@
+package edu.joyful.orderservice.command.api.model.event;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventsHandler {
+
+    @EventHandler
+    public void on(OrderCreatedEvent event) {
+        // TODO: 28.02.2023  
+    }
+}
