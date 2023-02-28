15f9a54495c7137ad22cdc08a2cd261f907ad6fd
Александр Плохой <alexplohoy@mail.ru>
2023-02-28 15:34:29 +0300

    add order-service dto

--- order-service/src/main/java/edu/joyful/orderservice/command/api/model/OrderRestModel.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/OrderRestModel.java
@@ -1,4 +1,15 @@
 package edu.joyful.orderservice.command.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRestModel {
+    private String productId;
+    private String userId;
+    private String addressId;
+    private Integer quantity;
 }
