1ce583f7ef3660cea13017e2dc1ceff35175982a
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 19:13:53 +0300

    add order-service complete command

---
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/command/CompleteOrderCommand.java
@@ -0,0 +1,14 @@
+package edu.joyful.orderservice.command.api.model.command;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class CompleteOrderCommand {
+
+    @TargetAggregateIdentifier
+    private String orderId;
+    private String orderStatus;
+}
