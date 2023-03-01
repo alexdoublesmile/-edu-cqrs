598d6e8c25eff0d663a20f82bebeea1e340d6c07
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:51:39 +0300

    add shipment dto target id

--- common-service/src/main/java/edu/joyful/commonservice/api/shipment/command/ShipOrderCommand.java
+++ common-service/src/main/java/edu/joyful/commonservice/api/shipment/command/ShipOrderCommand.java
@@ -2,11 +2,13 @@ package edu.joyful.commonservice.api.shipment.command;
 
 import lombok.Builder;
 import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class ShipOrderCommand {
 
+    @TargetAggregateIdentifier
     private String shipmentId;
     private String orderId;
 }
