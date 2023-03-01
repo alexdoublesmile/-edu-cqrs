c5409efd7b6036549adac119ed129a51e8c9c170
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:55:14 +0300

    add shipment-service command handler

--- common-service/src/main/java/edu/joyful/commonservice/api/shipment/event/OrderShippedEvent.java
+++ common-service/src/main/java/edu/joyful/commonservice/api/shipment/event/OrderShippedEvent.java
@@ -6,4 +6,8 @@ import lombok.Data;
 @Data
 @Builder
 public class OrderShippedEvent {
+
+    private String shipmentId;
+    private String orderId;
+    private String shipmentStatus;
 }
--- shipment-service/src/main/java/edu/joyful/shipmentservice/model/aggregate/ShipmentAggregate.java
+++ shipment-service/src/main/java/edu/joyful/shipmentservice/model/aggregate/ShipmentAggregate.java
@@ -1,9 +1,11 @@
 package edu.joyful.shipmentservice.model.aggregate;
 
 import edu.joyful.commonservice.api.shipment.command.ShipOrderCommand;
+import edu.joyful.commonservice.api.shipment.event.OrderShippedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @Slf4j
@@ -20,5 +22,12 @@ public class ShipmentAggregate {
 
     @CommandHandler
     public ShipmentAggregate(ShipOrderCommand command) {
+        final OrderShippedEvent event = OrderShippedEvent.builder()
+                .orderId(command.getOrderId())
+                .shipmentId(command.getShipmentId())
+                .shipmentStatus("COMPLETED")
+                .build();
+
+        AggregateLifecycle.apply(event);
     }
 }
