fb72865ead7e641004984895e68ffd5e6a8486c4
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:51:08 +0300

    add shipment-service aggregate

---
+++ shipment-service/src/main/java/edu/joyful/shipmentservice/model/aggregate/ShipmentAggregate.java
@@ -0,0 +1,24 @@
+package edu.joyful.shipmentservice.model.aggregate;
+
+import edu.joyful.commonservice.api.shipment.command.ShipOrderCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Slf4j
+@Aggregate
+public class ShipmentAggregate {
+
+    @AggregateIdentifier
+    private String shipmentId;
+    private String orderId;
+    private String shipmentStatus;
+
+    public ShipmentAggregate() {
+    }
+
+    @CommandHandler
+    public ShipmentAggregate(ShipOrderCommand command) {
+    }
+}
