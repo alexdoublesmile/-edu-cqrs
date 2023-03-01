325b79deceb047fd5a549e00d8cc21e820ff03df
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 12:27:13 +0300

    add user query handler

--- user-service/src/main/java/edu/joyful/userservice/model/UserProjection.java
+++ user-service/src/main/java/edu/joyful/userservice/model/UserProjection.java
@@ -1,7 +1,29 @@
 package edu.joyful.userservice.model;
 
+import edu.joyful.commonservice.api.payment.dto.CardDetails;
+import edu.joyful.commonservice.api.user.dto.UserDto;
+import edu.joyful.commonservice.api.user.query.GetUserPaymentDetailsQuery;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserProjection {
+
+    @QueryHandler
+    public UserDto getUserPaymentDetails(GetUserPaymentDetailsQuery query) {
+        final CardDetails card = CardDetails.builder()
+                .cardNumber("123456")
+                .name("Alex")
+                .validUntilMonth(01)
+                .validUntilYear(2022)
+                .cvv(111)
+                .build();
+
+        UserDto.builder()
+                .userId(query.getUserId())
+                .firstName("Alex")
+                .lastName("Smith")
+                .cardDetails(card)
+                .build();
+    }
 }
