50f2ff80118a1e9872e8085fbc51447193351d48
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:23:48 +0300

    add payment-service aggregate

---
+++ payment-service/src/main/java/edu/joyful/paymentservice/model/PaymentAggregate.java
@@ -0,0 +1,24 @@
+package edu.joyful.paymentservice.model;
+
+import edu.joyful.commonservice.api.payment.command.ValidatePaymentCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Slf4j
+@Aggregate
+public class PaymentAggregate {
+
+    @AggregateIdentifier
+    private String paymentId;
+    private String orderId;
+    private String paymentStatus;
+
+    public PaymentAggregate() {
+    }
+
+    @CommandHandler
+    public PaymentAggregate(ValidatePaymentCommand command) {
+    }
+}
