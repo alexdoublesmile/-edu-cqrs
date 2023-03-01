e23e944db3bb79ad380200610ac28f099c9be748
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:35:17 +0300

    add payment-service event sourcing handler

--- payment-service/src/main/java/edu/joyful/paymentservice/model/PaymentAggregate.java
+++ payment-service/src/main/java/edu/joyful/paymentservice/model/PaymentAggregate.java
@@ -4,6 +4,7 @@ import edu.joyful.commonservice.api.payment.command.ValidatePaymentCommand;
 import edu.joyful.commonservice.api.payment.event.PaymentProcessedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -33,5 +34,11 @@ public class PaymentAggregate {
 
         AggregateLifecycle.apply(event);
         log.info("PaymentProcessedEvent Applied");
+    }
+
+    @EventSourcingHandler
+    public void on(PaymentProcessedEvent event) {
+        paymentId = event.getPaymentId();
+        orderId = event.getOrderId();
     }
 }
