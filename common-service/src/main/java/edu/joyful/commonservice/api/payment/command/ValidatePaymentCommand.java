3620288892d91b9d889dea9761f82a937c2db50f
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 18:18:14 +0300

    add common api aggregate id

--- common-service/src/main/java/edu/joyful/commonservice/api/payment/command/ValidatePaymentCommand.java
+++ common-service/src/main/java/edu/joyful/commonservice/api/payment/command/ValidatePaymentCommand.java
@@ -3,11 +3,13 @@ package edu.joyful.commonservice.api.payment.command;
 import edu.joyful.commonservice.api.payment.CardDetails;
 import lombok.Builder;
 import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class ValidatePaymentCommand {
 
+    @TargetAggregateIdentifier
     private String paymentId;
     private String orderId;
     private CardDetails cardDetails;
