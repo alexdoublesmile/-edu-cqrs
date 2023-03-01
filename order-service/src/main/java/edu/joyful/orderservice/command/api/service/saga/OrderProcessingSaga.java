31049a389c092accf3e90707ecc8eed88f4fde71
Александр Плохой <alexplohoy@mail.ru>
2023-03-01 17:51:50 +0300

    add order-service saga payment command

--- order-service/src/main/java/edu/joyful/orderservice/command/api/service/saga/OrderProcessingSaga.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/service/saga/OrderProcessingSaga.java
@@ -1,5 +1,6 @@
 package edu.joyful.orderservice.command.api.service.saga;
 
+import edu.joyful.commonservice.api.payment.command.ValidatePaymentCommand;
 import edu.joyful.commonservice.api.user.dto.UserDto;
 import edu.joyful.commonservice.api.user.query.GetUserPaymentDetailsQuery;
 import edu.joyful.orderservice.command.api.model.event.OrderCreatedEvent;
@@ -11,6 +12,8 @@ import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
+
+import static java.util.UUID.randomUUID;
 
 @Slf4j
 @Saga
@@ -36,10 +39,18 @@ public class OrderProcessingSaga {
             // TODO: 01.03.2023 start compensation transaction
         }
 
-        // TODO: 28.02.2023 validate payment command create
+        final ValidatePaymentCommand paymentCommand = ValidatePaymentCommand.builder()
+                .orderId(event.getOrderId())
+                .paymentId(randomUUID().toString())
+                .cardDetails(userDto.getCardDetails())
+                .build();
 
+        commandGateway.sendAndWait(paymentCommand);
+
+        // TODO: 01.03.2023 handle paymentCommand in payment handler
         // TODO: 28.02.2023 get shipping info from shipment-service
         // TODO: 28.02.2023 ship command create
+        // TODO: 01.03.2023 handle ship command in shipment handler
 
         // TODO: 28.02.2023 complete order command
 
