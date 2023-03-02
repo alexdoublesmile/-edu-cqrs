12632314430294e42d9920288ee727eb8bbd90c2
Александр Плохой <alexplohoy@mail.ru>
2023-03-02 10:35:54 +0300

    fix order

--- order-service/src/main/java/edu/joyful/orderservice/command/api/model/entity/Order.java
+++ order-service/src/main/java/edu/joyful/orderservice/command/api/model/entity/Order.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "orders")
 public class Order {
 
     @Id
