24f4fc6faf84b601d7c5d3ee794315a049c53296
Александр Плохой <alexplohoy@mail.ru>
2023-03-03 12:51:31 +0300

    add cancel payment event apply

--- payment-service/src/main/java/edu/joyful/paymentservice/model/PaymentAggregate.java
+++ payment-service/src/main/java/edu/joyful/paymentservice/model/PaymentAggregate.java
@@ -1,6 +1,8 @@
 package edu.joyful.paymentservice.model;
 
+import edu.joyful.commonservice.api.payment.command.CancelPaymentCommand;
 import edu.joyful.commonservice.api.payment.command.ValidatePaymentCommand;
+import edu.joyful.commonservice.api.payment.event.PaymentCancelledEvent;
 import edu.joyful.commonservice.api.payment.event.PaymentProcessedEvent;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
 
 @Slf4j
 @Aggregate
@@ -33,6 +36,20 @@ public class PaymentAggregate {
 
         AggregateLifecycle.apply(event);
         log.info("PaymentProcessedEvent Applied");
+    }
+
+    @CommandHandler
+    public void handle(CancelPaymentCommand command) {
+        log.info("Executing CancelPaymentCommand for orderId: {} and paymentId: {}",
+                command.getOrderId(), command.getPaymentId());
+        // TODO: 01.03.2023 validate payment details
+
+        PaymentCancelledEvent event = new PaymentCancelledEvent();
+
+        BeanUtils.copyProperties(command, event);
+
+        AggregateLifecycle.apply(event);
+        log.info("PaymentCancelledEvent Applied");
     }
 
     @EventSourcingHandler
