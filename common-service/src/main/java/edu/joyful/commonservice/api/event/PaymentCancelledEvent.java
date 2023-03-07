cf1627fe9a5311c167b6b09a763d4d6047486242
Александр Плохой <alexplohoy@mail.ru>
2023-03-07 19:22:53 +0300

    fix common api

--- common-service/src/main/java/edu/joyful/commonservice/api/event/PaymentCancelledEvent.java
+++ common-service/src/main/java/edu/joyful/commonservice/api/event/PaymentCancelledEvent.java
@@ -1,10 +1,14 @@
 package edu.joyful.commonservice.api.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentCancelledEvent {
 
     private String paymentId;
